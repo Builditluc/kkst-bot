10dc65a51ed51f62b90141f0750b2589f2dce225
Builditluc <37375448+Builditluc@users.noreply.github.com>
2021-12-05 21:38:38 +0100

    feat(bot.utils): add wait_for util function

--- src/bot/utils.py
+++ src/bot/utils.py
@@ -1,5 +1,7 @@
 from typing import List
+from datetime import time, timedelta
 from nextcord.message import Message
+from nextcord.utils import utcnow, sleep_until
 
 from bot.logging import get_logger
 
@@ -14,3 +16,8 @@ async def delete_message(message: Message):
 async def cleanup(messages: List[Message]):
     for message in messages:
         await delete_message(message)
+
+async def wait_for(days=0, seconds=0, microseconds=0):
+    log.debug(f"waiting for days: {days} seconds: {seconds} microseconds: {microseconds}")
+    when = utcnow() + timedelta(days, seconds, microseconds)
+    await sleep_until(when)

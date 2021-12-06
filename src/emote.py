cbe696f9b42689a1557c0f0064a9e109dea438c1
Builditluc <37375448+Builditluc@users.noreply.github.com>
2021-12-06 21:17:13 +0100

    feat(emote): add emotes

---
+++ src/emote.py
@@ -0,0 +1,25 @@
+from typing import Dict
+from dataclasses import dataclass
+from nextcord.partial_emoji import PartialEmoji
+
+@dataclass
+class Emote:
+    unicode: str
+
+    def emoji(self) -> PartialEmoji:
+        return PartialEmoji(name=self.unicode)
+
+    def __eq__(self, other: object) -> bool:
+        return super().__eq__(other)
+
+
+CHECK: Dict[str, Emote] = {
+    "white_check_mark": Emote("✅"),
+    "x": Emote("❌"),
+}
+
+NUMBERS: Dict[str, Emote] = {
+    "one": Emote("1️⃣"),
+    "two": Emote("2️⃣"),
+    "three": Emote("3️⃣"),
+}

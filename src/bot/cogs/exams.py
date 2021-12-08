c64dd9b4f6201093986993627c399ed922efb51e
Builditluc <37375448+Builditluc@users.noreply.github.com>
2021-12-08 17:09:09 +0100

    feat(bot.cogs): add exams._edit_exam_topics

--- src/bot/cogs/exams.py
+++ src/bot/cogs/exams.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import List, Tuple, Union, cast
+from typing import List, Optional, Tuple, Union, cast
 from datetime import datetime
 from nextcord.channel import TextChannel
 from nextcord.embeds import Embed
@@ -21,19 +21,71 @@ log = get_logger("kkst-bot")
 
 # TODO: replace this with database interface later
 @dataclass
+class ExamTopic:
+    title: str
+    content: List[str]
+
+@dataclass
 class Exam():
     name: str
     date: datetime
+    topics: List[ExamTopic]
     message_id: int
+
+def exam_topics_from_string(topics_string: str) -> List[ExamTopic]:
+    """
+    parse strings in this format:
+
+    [t]Topic 1:
+    [c]- topic 1.1
+    [c]- topic 1.2
+    [t]Topic 2:
+    ...
+    """
+    topics: List[ExamTopic] = []
+    current_topic: Optional[ExamTopic] = None
+
+    for string in topics_string.splitlines(keepends=False):
+        if string.startswith("[t]"):
+            if current_topic is not None:
+                topics.append(current_topic)
+                current_topic = None
+            current_topic = ExamTopic(title=string.lstrip("[t]"), content=[])
+        elif string.startswith("[c]"):
+            if current_topic is None:
+                continue
+            current_topic.content.append(string.lstrip("[c]"))
+
+    if current_topic is not None:
+        topics.append(current_topic)
+
+    return topics
 
 exams: List[Exam] = []
 
 def create_embed_from_exam(exam: Exam) -> Embed:
-    return Embed(
+    embed = Embed(
         title=exam.name,
         type="rich",
         timestamp=exam.date
     )
+
+    topics_without_content: List[str] = []
+
+    for topic in exam.topics:
+        if len(topic.content) == 0:
+            topics_without_content.append(topic.title)
+            continue
+
+        embed.add_field(
+            name=topic.title,
+            value="\n".join(topic.content)
+        )
+
+    if len(topics_without_content) > 0:
+        embed.insert_field_at(0, name="Themen:", value="\n".join(topics_without_content))
+
+    return embed
 
 
 class Exams(commands.Cog):
@@ -61,7 +113,7 @@ class Exams(commands.Cog):
             await ctx.message.add_reaction(emote.CHECK["x"].emoji)
             return await cleanup(messages)
 
-        exam = Exam(exam_name, exam_date, 0)
+        exam = Exam(exam_name, exam_date, [], 0)
 
         msg: Message = await ctx.send(
             content="Here is a preview of the Exam. Is everything correct?",
@@ -163,7 +215,7 @@ class Exams(commands.Cog):
 
         {select_options[0]}: Name
         {select_options[1]}: Date
-        {select_options[2]}: Topics (WIP)
+        {select_options[2]}: Topics
         """)
         messages.append(msg)
 
@@ -176,24 +228,25 @@ class Exams(commands.Cog):
             return await cleanup(messages)
 
         elif reaction == 0:
-            is_error, msgs = await self._edit_exam_name(ctx, exam)
+            no_error, msgs = await self._edit_exam_name(ctx, exam)
             messages.extend(msgs)
-            if is_error is True:
+            if no_error is True:
                 await ctx.message.add_reaction(emote.CHECK["white_check_mark"].emoji)
                 return await cleanup(messages)
 
         elif reaction == 1:
-            is_error, msgs = await self._edit_exam_date(ctx, exam)
+            no_error, msgs = await self._edit_exam_date(ctx, exam)
             messages.extend(msgs)
-            if is_error is True:
+            if no_error is True:
                 await ctx.message.add_reaction(emote.CHECK["white_check_mark"].emoji)
                 return await cleanup(messages)
 
         elif reaction == 2:
-            msg: Message = await ctx.send("Sorry, that's still in development :/")
-            messages.append(msg)
-
-            await wait_for(seconds=4)
+            no_error, msgs = await self._edit_exam_topics(ctx, exam)
+            messages.extend(msgs)
+            if no_error is True:
+                await ctx.message.add_reaction(emote.CHECK["white_check_mark"].emoji)
+                return await cleanup(messages)
 
         await ctx.message.add_reaction(emote.CHECK["x"].emoji)
         return await cleanup(messages)
@@ -222,7 +275,6 @@ class Exams(commands.Cog):
         return (False, messages)
 
     async def _edit_exam_date(self, ctx: commands.Context, exam: Exam) -> Tuple[bool, List[Message]]:
-        # TODO: implement Exams._edit_exam_name
         messages: List[Message] = []
 
         utils_cog = cast(Utils, self.bot.get_cog("utils"))
@@ -235,6 +287,41 @@ class Exams(commands.Cog):
         for i, _exam in enumerate(exams):
             if _exam.message_id == exam.message_id:
                 exams[i].date = date
+
+                is_error = await self._update_exam(_exam)
+                if is_error:
+                    break
+
+                return (True, messages)
+
+        return (False, messages)
+
+    async def _edit_exam_topics(self, ctx: commands.Context, exam: Exam) -> Tuple[bool, List[Message]]:
+        messages: List[Message] = []
+
+        utils_cog = cast(Utils, self.bot.get_cog("utils"))
+        topics_string, msgs = await utils_cog.get_string(ctx, "Please enter the topics in the correct format down below")
+        messages.extend(msgs)
+
+        if topics_string is None:
+            return (False, messages)
+
+        exam.topics = exam_topics_from_string(topics_string)
+        exam_preview = await ctx.send(
+            content="Here is a preview of the Exam. Is everything correct?",
+            embed=create_embed_from_exam(exam)
+        )
+        messages.append(exam_preview)
+
+        reaction, msgs = await utils_cog.get_reaction(ctx, exam_preview, list(emote.CHECK.values()), ctx.author)
+        messages.extend(msgs)
+
+        if reaction is None or reaction == 1:
+            return (False, messages)
+
+        for i, _exam in enumerate(exams):
+            if _exam.message_id == exam.message_id:
+                exams[i].topics = exam.topics
 
                 is_error = await self._update_exam(_exam)
                 if is_error:
